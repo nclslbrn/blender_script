f050d0e09b9b68f83ea4c943bf424110c0254094
Nicolas Lebrun <nlebrun@y50.local>
2019-11-24 16:38:58 +0100

    add radius parameter to function

--- random-curve.py
+++ random-curve.py
@@ -1,11 +1,14 @@
 import bpy
 import random
+import math
 from mathutils import *
 D = bpy.data
 C = bpy.context
 
+'''
+    Delete everythings in the scene
 
-# Delete everythings in the scene
+'''
 objs = []
 for obj in C.scene.objects:
     objs.append(obj)
@@ -16,12 +19,20 @@ bpy.ops.object.delete({"selected_objects": objs})
 
 '''
 
-def MakeRandomCurve(objName, curveName, numPoint, scale):
-    # Setup curve 
-    curveData = D.curves.new(name=curveName, type='CURVE')
-    curveData.bevel_depth = 0.05
-    curveData.dimensions = '3D'
+def MakeRandomCurve(objName, curveName, numPoint, scale, radius):
 
+    curveRes = 32
+    extrude = 0.01
+
+    # Setup curve path
+    curveData = D.curves.new(name=curveName, type='CURVE')
+    curveData.bevel_depth = radius
+    curveData.dimensions = '3D'
+    curveData.resolution_u = curveRes
+    curveData.render_resolution_u = curveRes
+    curveData.extrude = extrude
+
+    # Setup curve object
     curve = bpy.data.objects.new(objName, curveData)  
     curve.location = (0,0,0)
     C.scene.collection.objects.link(curve)
@@ -29,11 +40,16 @@ def MakeRandomCurve(objName, curveName, numPoint, scale):
     polyline = curveData.splines.new('BEZIER')
     polyline.bezier_points.add(numPoint-1)
 
+    x = 0
+    y = 0
+    z = 0
+
     for p in range(0, numPoint):
+
         x = random.randint(-scale, scale)
         y = random.randint(-scale, scale)
         z = random.randint(-scale, scale)
-        
+
         polyline.bezier_points[p].co = [x, y, z]
 
     polyline.order_u = len(polyline.points)-1
@@ -43,4 +59,4 @@ def MakeRandomCurve(objName, curveName, numPoint, scale):
     
     return curve
 
-curve = MakeRandomCurve('curveObject', 'curve', 12, 5)
+curve = MakeRandomCurve('curveObject', 'curve', 256, 5, 0.01)
