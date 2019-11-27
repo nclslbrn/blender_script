caf349b7024abe5cd1ece18d2150db3701993fc6
Nicolas Lebrun <nlebrun@y50.local>
2019-11-27 22:56:55 +0100

    TypeError: 'str' object is not callable

---
+++ dla.py
@@ -0,0 +1,142 @@
+import bpy
+import random
+import bmesh
+import numpy
+import scipy
+from math import sin, cos, pi, sqrt
+from mathutils import Vector
+
+# Convenience Variables
+D = bpy.data
+C = bpy.context
+
+# Delete everythings in the scene
+objs = []
+for obj in C.scene.objects:
+    objs.append(obj)
+bpy.ops.object.delete({"selected_objects": objs})
+
+'''
+    Your creative code here
+
+'''
+size = 12
+pointPos = { 'x': 0, 'y': 0, 'z': 0 }
+pointHistory = []
+nPoint = 0
+step = 15
+
+quarterPi = pi / 4
+
+'''
+    MOOVE FUNCTION
+'''
+
+def NN(point):
+    point['x'] -= step
+    return point
+
+def SS(point):
+    point['x'] += step
+    return point
+
+def WW(point):
+    point['y'] -= step
+    return point
+
+def EE(point):
+    point['y'] += step
+    return point
+
+def NE(point):
+    point['x'] += step * cos(quarterPi) 
+    point['y'] -= step * sin(quarterPi)
+    return point
+
+def NW(point):
+    point['x'] -= step * cos(quarterPi)
+    point['y'] -= step * sin(quarterPi)
+    return point
+
+def SE(point):
+    point['x'] += step * cos(quarterPi)
+    point['y'] += step * sin(quarterPi)
+    return point
+
+def SW(point):
+    point['x'] -= step * cos(quarterPi)
+    point['y'] += step * sin(quarterPi)
+    return point
+
+moves = {
+    0 : 'NN', 
+    1 : 'NE',
+    2 : 'EE', 
+    3 : 'SE',
+    4 : 'SS',
+    5 : 'SW',
+    6 : 'WW',
+    7 : 'NW'
+}
+
+'''
+    SETUP OBJECT
+'''
+
+def setupCurve():
+    curveRes = 32
+    extrude = 0.01
+    # Setup curve path
+    curveData = D.curves.new(name='curveName', type='CURVE')
+    curveData.bevel_depth = 0.01
+    curveData.dimensions = '3D'
+    curveData.resolution_u = curveRes
+    curveData.render_resolution_u = curveRes
+    curveData.extrude = extrude
+
+    # Setup curve object
+    curve = bpy.data.objects.new('objName', curveData)  
+    curve.location = (0,0,0)
+    C.scene.collection.objects.link(curve)
+
+    polyline = curveData.splines.new('BEZIER')
+
+    return polyline
+
+def drawCurve(curve, pointHistory):
+
+    polyline.bezier_points.add(len(pointHistory-1))
+
+    for p in range(0, len(pointHistory)):
+
+        polyline.bezier_points[p].co = [
+            pointHistory[p]['x'], 
+            pointHistory[p]['y'],
+            pointHistory[p]['z']
+        ]
+
+    polyline.order_u = len(polyline.points)-1
+    polyline.use_endpoint_u = True
+    polyline.use_cyclic_u = True 
+
+
+'''
+    MOOVE LOOP
+'''
+
+polyline = setupCurve()
+
+while( 
+    pointPos['x'] > size * -0.5 or 
+    pointPos['y'] > size * -0.5 or
+    pointPos['x'] < size * 0.5 or
+    pointPos['y'] < size * 0.5 
+):
+    move = int(random.choice(list(moves.keys())))
+    print(move)
+    pointPos = moves[move](pointPos)
+    pointHistory[nPoint].co = [pointPos['x'], pointPos['y'], pointPos.z]
+    drawCurve(polyline, pointHistory)
+    nPoint+=1
+
+
