d4e6c7e2b3ed4d4062ece0ebed6dc0efcf3775b1
Nicolas Lebrun <nclslbrn@gmail.com>
2019-12-30 11:54:49 +0100

    possible type comment to pass to cleanScene

--- functions/cleanScene.py
+++ functions/cleanScene.py
@@ -1,5 +1,9 @@
 import bpy  # noqa
 
+# Delete every object of a specified type
+# Type: MESH, CURVE, SURFACE, META, FONT, ARMATURE, LATTICE,
+# EMPTY, GPENCIL, CAMERA, LIGHT, SPEAKER, LIGHT_PROBE
+
 
 def cleanScene(type_to_delete='MESH'):
     objs = []
